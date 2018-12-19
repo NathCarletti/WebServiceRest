b3410578176e63029ad42b70e572adb70d460790
Nath <ncsoares2@gmail.com>
2018-12-19 17:54:02 -0200

    azurePT1

--- Projeto/Projeto/Controllers/OrdersController.cs
+++ Projeto/Projeto/Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,72 +13,66 @@ using Projeto.Models;
 
 namespace Projeto.Controllers
 {
+    [Authorize]
     public class OrdersController : ApiController
     {
         private ProjetoContext db = new ProjetoContext();
 
         // GET: api/Orders
+        //Listar todos apenas ADMIN
+        [Authorize(Roles = "ADMIN")]
         public IQueryable<Order> GetOrders()
         {
             return db.Orders;
         }
 
         // GET: api/Orders/5
+        //Lista pedido pelo Id - ADMIM ou dono do pedido
         [ResponseType(typeof(Order))]
-        public IHttpActionResult GetOrder(int id)
+        public IHttpActionResult GetOrderById(int id)
         {
             Order order = db.Orders.Find(id);
             if (order == null)
             {
-                return NotFound();
+                return BadRequest("O pedido não existe!");
             }
-
-            return Ok(order);
+            if (IsAuthorized(order.Email)) return Ok(order); else return BadRequest("Usuário não autorizado!");
         }
 
-        // PUT: api/Orders/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutOrder(int id, Order order)
+        // GET: api/Orders/byEmail?email={email}
+        // Lista pedidos do usuário pelo email - ADMIN ou dono        
+        [ResponseType(typeof(Order))]
+        [HttpGet]
+        [Route("api/Orders/byEmail")]
+        public IHttpActionResult GetOrdersByEmail(string email)
         {
-            if (!ModelState.IsValid)
+            if (IsAuthorized(email))
             {
-                return BadRequest(ModelState);
-            }
+                List<Order> orders = db.Orders.Where(order => order.Email.ToLower().Trim().Equals(email.ToLower().Trim())).ToList();
 
-            if (id != order.OrderId)
-            {
-                return BadRequest();
-            }
+                if (orders == null) return BadRequest("Usuário não possui pedidos.");
 
-            db.Entry(order).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
+                return Ok(orders);
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
+            else return BadRequest("Usuário não autorizado.");
         }
 
         // POST: api/Orders
+        //Apenas usuários autenticados criam pedidos
         [ResponseType(typeof(Order))]
         public IHttpActionResult PostOrder(Order order)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            
+            //Valores pré-definidos
+            order.Status = "novo";
+            order.PesoTotal = 0;
+            order.PrecoFrete = 0;
+            order.PrecoTotal = 0;
+            order.DataEntrega = DateTime.Now;
+
+            //Retorna o email do usuário autenticado
+            order.Email = User.Identity.Name;
 
             db.Orders.Add(order);
             db.SaveChanges();
@@ -85,20 +80,50 @@ namespace Projeto.Controllers
             return CreatedAtRoute("DefaultApi", new { id = order.OrderId }, order);
         }
 
+
+        // PUT: api/Orders/close/5
+        //Pedido apenas fecha caso frete já tenha sido calculado - ADMIN ou Dono do pedido
+        [ResponseType(typeof(void))]
+        [Route("api/Orders/close")]
+        public IHttpActionResult PutOrder(int id)
+        {
+            Order order = db.Orders.Find(id);
+
+            if (order == null) return BadRequest("O pedido não existe!");
+            
+            if (IsAuthorized(order.Email))
+            {
+                if (order.PrecoFrete != 0)
+                {
+                    order.Status = "fechado";
+                    db.Entry(order).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return Ok(order);
+                }
+                else return BadRequest("Frete não calculado!");
+            }
+            else return BadRequest("Usuário não autorizado!");
+        }
+
+     
         // DELETE: api/Orders/5
+        //Deleta pedido pelo Id apenas pelo ADM e Dono do pedido
         [ResponseType(typeof(Order))]
         public IHttpActionResult DeleteOrder(int id)
         {
             Order order = db.Orders.Find(id);
             if (order == null)
             {
-                return NotFound();
+                return BadRequest("O pedido não existe!");
             }
-
-            db.Orders.Remove(order);
-            db.SaveChanges();
-
-            return Ok(order);
+            if (IsAuthorized(order.Email))
+            {
+                db.Orders.Remove(order);
+                db.SaveChanges();
+                return Ok(order);
+            }
+            else return BadRequest("Usuário não autorizado!");
         }
 
         protected override void Dispose(bool disposing)
@@ -113,6 +138,15 @@ namespace Projeto.Controllers
         private bool OrderExists(int id)
         {
             return db.Orders.Count(e => e.OrderId == id) > 0;
+        }
+        //Verifica se email é de usuário logado
+        private bool IsAuthorized(string email)
+        {
+            bool userOwnOrder = User.Identity.Name.Equals(email);
+
+            Trace.TraceInformation(User.Identity.Name);
+
+            return (User.IsInRole("ADMIN") || userOwnOrder);
         }
     }
 }
--- Projeto/Projeto/Controllers/ProductsController.cs
+++ Projeto/Projeto/Controllers/ProductsController.cs
@@ -17,37 +17,67 @@ namespace Projeto.Controllers
         private ProjetoContext db = new ProjetoContext();
 
         // GET: api/Products
+        [Authorize(Roles = "ADMIN,USER")]
         public IQueryable<Product> GetProducts()
         {
             return db.Products;
         }
 
         // GET: api/Products/5
+        [Authorize(Roles = "ADMIN,USER")]
         [ResponseType(typeof(Product))]
         public IHttpActionResult GetProduct(int id)
         {
             Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
 
-            return Ok(product);
+            if (product == null) return NotFound();
+
+            bool prodSameCode = db.Products.Any(prod => prod.codigo == product.codigo);
+            bool prodSameModel = db.Products.Any(prod => prod.modelo == product.modelo);
+
+            if (prodSameCode && prodSameModel) return BadRequest("Código e modelo já existente.");
+            else if (prodSameCode) return BadRequest("Código já existente.");
+            else if (prodSameModel) return BadRequest("Modelo já existente.");
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
+        }
+
+        // POST: api/Products
+        [Authorize(Roles = "ADMIN")]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
         }
 
         // PUT: api/Products/5
+        //Não altera modelo e código para valores existentes
+        [Authorize(Roles = "ADMIN")]
         [ResponseType(typeof(void))]
         public IHttpActionResult PutProduct(int id, Product product)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            if (id != product.Id)
-            {
-                return BadRequest();
-            }
+            if (id != product.Id) return BadRequest();
+
+            bool prodSameCode = db.Products.Where(prod => prod.codigo != product.codigo)
+                .Any(prod => prod.codigo == product.codigo);
+            bool prodSameModel = db.Products.Where(prod => prod.modelo != product.modelo)
+                .Any(prod => prod.modelo == product.modelo);
+
+            if (prodSameCode && prodSameModel) return BadRequest("Código e modelo já existente.");
+            
+            else if (prodSameCode) return BadRequest("Código já existente.");
+            
+            else if (prodSameModel) return BadRequest("Modelo já existente.");
 
             db.Entry(product).State = EntityState.Modified;
 
@@ -57,42 +87,22 @@ namespace Projeto.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!ProductExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                if (!ProductExists(id)) return NotFound();
+                else throw;
             }
-
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/Products
-        [ResponseType(typeof(Product))]
-        public IHttpActionResult PostProduct(Product product)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Products.Add(product);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
-        }
 
         // DELETE: api/Products/5
+        [Authorize(Roles = "ADMIN")]
         [ResponseType(typeof(Product))]
         public IHttpActionResult DeleteProduct(int id)
         {
             Product product = db.Products.Find(id);
             if (product == null)
             {
-                return NotFound();
+                return BadRequest("Produto não encontrado!");
             }
 
             db.Products.Remove(product);
